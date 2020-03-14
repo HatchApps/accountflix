7d2ac4ecd7e08b39618125d926031dbeab26f621
Rich Nicolson <richard.nicolson@gmail.com>
2020-03-14 22:09:09 +0000

    Configure eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,17 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
+  plugins: [
+    'vue'
+  ],
   extends: [
     '@nuxtjs',
     'plugin:nuxt/recommended'
   ],
   // add your custom rules here
   rules: {
+      'indent': ['error', 4],
+      'vue/html-indent': ['error', 4],
+      'vue/script-indent': ['error', 4],
   }
 }
